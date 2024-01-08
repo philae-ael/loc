928a214e1885c8d966aca3aec7ac5e11fa971e8a
philae-ael <1511446+philae-ael@users.noreply.github>
2024-01-08 23:52:59 +0100

    C -> C++

--- src/language.rs
+++ src/language.rs
@@ -20,7 +20,7 @@ impl std::fmt::Display for Language {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let this = match self {
             Language::Rust => "Rust",
-            Language::C => "C",
+            Language::C => "C/C++",
             Language::Generic => "Other",
             Language::Js => "Javascript",
             Language::Json => "JSON",
