760f0f07ea772288801817292153b9abd942c01d
philae-ael <1511446+philae-ael@users.noreply.github.com>
2025-01-25 18:47:18 +0100

    remove macro for a big match

--- src/identify.rs
+++ src/identify.rs
@@ -4,19 +4,14 @@ use crate::language::Language;
 
 pub fn identify(path: &Path, debug: bool) -> Language {
     if let Some(filename) = path.file_name().and_then(|x| x.to_str()) {
-        macro_rules! by_filename {
-            ($lang:expr, $($id:expr),+) => {
-                if [$($id),*].iter().any(|x| &filename == x) {
-                    return $lang;
-                }
-            };
+        match filename {
+            "Dockerfile" => return Language::Dockerfile,
+            "CMakeLists.txt" => return Language::CMake,
+            "Makefile" => return Language::Makefile,
+            "LICENSE" => return Language::Txt,
+            "Cargo.lock" => return Language::Txt,
+            _ => (),
         }
-
-        by_filename!(Language::Dockerfile, "Dockerfile");
-        by_filename!(Language::CMake, "CMakeLists.txt");
-        by_filename!(Language::Makefile, "Makefile");
-        by_filename!(Language::Txt, "LICENSE");
-        by_filename!(Language::Txt, "Cargo.lock");
     }
 
     if let Some(extension) = path
@@ -24,54 +19,32 @@ pub fn identify(path: &Path, debug: bool) -> Language {
         .and_then(|x| x.to_str())
         .map(|x| x.to_lowercase())
     {
-        macro_rules! by_extension {
-            ($lang:expr, $($id:ident),+) => {
-                if [$(stringify!($id)),*].iter().any(|x| &extension == x) {
-                    return $lang;
-                }
-            };
+        match extension.as_str() {
+            "c" | "h" | "cpp" | "hpp" => return Language::C,
+            "vert" | "frag" | "glsl" => return Language::Shader,
+            "rs" => return Language::Rust,
+            "py" => return Language::Python,
+            "js" => return Language::Js,
+            "json" => return Language::Json,
+            "toml" => return Language::Toml,
+            "go" => return Language::Go,
+            "csv" => return Language::Csv,
+            "yaml" | "yml" => return Language::Yaml,
+            "scss" | "css" => return Language::Css,
+            "html" => return Language::Html,
+            "vue" => return Language::VueJs,
+            "md" => return Language::Markdown,
+            "tex" | "bib" => return Language::Tex,
+            "sh" | "bash" | "zsh" | "fish" => return Language::Shell,
+            "txt" => return Language::Txt,
+            "rb" => return Language::Ruby,
+            "liquid" => return Language::Liquid,
+            "slang" => return Language::Slang,
+            "jpg" | "png" | "jpeg" | "gif" | "bmp" | "ttf" | "pdf" | "obj" | "mtl" | "woff"
+            | "woff2" | "o" | "bin" | "gltf" | "out" | "map" | "mp3" => return Language::Asset,
+            "cmake" => return Language::CMake,
+            _ => (),
         }
-
-        by_extension!(Language::C, c, h, cpp, hpp);
-        by_extension!(Language::Shader, vert, frag, glsl);
-        by_extension!(Language::Rust, rs);
-        by_extension!(Language::Python, py);
-        by_extension!(Language::Js, js);
-        by_extension!(Language::Json, json);
-        by_extension!(Language::Toml, toml);
-        by_extension!(Language::Go, go);
-        by_extension!(Language::Csv, csv);
-        by_extension!(Language::Yaml, yaml, yml);
-        by_extension!(Language::Css, scss, css);
-        by_extension!(Language::Html, html);
-        by_extension!(Language::VueJs, vue);
-        by_extension!(Language::Markdown, md);
-        by_extension!(Language::Tex, tex, bib);
-        by_extension!(Language::Shell, sh, bash, zsh, fish);
-        by_extension!(Language::Txt, txt);
-        by_extension!(Language::Ruby, rb);
-        by_extension!(Language::Liquid, liquid);
-        by_extension!(
-            Language::Asset,
-            jpg,
-            png,
-            jpeg,
-            gif,
-            bmp,
-            ttf,
-            pdf,
-            obj,
-            mtl,
-            woff,
-            woff2,
-            o,
-            bin,
-            gltf,
-            out,
-            map,
-            mp3
-        );
-        by_extension!(Language::CMake, cmake);
     }
 
     if debug {
